ab5c8e76c21d58a68ee2c40802a0fdf4d584a271
kariastnor <karin.noresten@live.com>
2022-08-30 13:01:18 +0200

    Fix incorrect photo on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 import styled from "styled-components";
 import InfoHome from "../components/InfoHome";
 import ContactButton from "../components/ContactButton";
-import tropicalBeach from "../data/photos/tropical-beach-2.JPG";
+import tropicalBeach from "../data/photos/tropical-beach-main.jpg";
 import Testimonies from "./Testimonies";
 
 function Home() {
